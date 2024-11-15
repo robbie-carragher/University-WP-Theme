cb4a181714b00fd89f21794c47a95facae6ac8a0
robbie-carragher <orcafinweb@gmail.com>
2024-11-15 14:04:17 -0800

    header and footer global completed

--- app/public/wp-content/themes/university-theme/single.php
+++ app/public/wp-content/themes/university-theme/single.php
@@ -1,9 +1,14 @@
-<?php 
-while(have_posts()) {
-the_post();?>
-<h2><?php the_title()?></a></h2>
-<p><?php the_content()?></p>
+<?php
+  
+  get_header();
 
-<?php }
+  while(have_posts()) {
+    the_post(); ?>
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+    
+  <?php }
+
+  get_footer();
 
 ?>
